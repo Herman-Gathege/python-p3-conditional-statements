63f1ec34527b5c0458a755667e0bd23e6bc191f4
Herman-Gathege <siinamtechstudio@gmail.com>
2024-08-29 21:17:06 +0300

    added now

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,54 @@
 
 def admin_login(username, password):
     # your code here
-    pass
+    if (username == "admin" or username == "ADMIN") and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
+result = admin_login("admin", "12345")
+print(result)     
+
 
 def hows_the_weather(temperature):
     # your code here
-    pass
+    if temperature < 40:
+        response = "brisk"
+    elif 40 <= temperature <= 65:
+        response = "a little chilly"
+    elif temperature > 85:
+        response = "too dang hot"
+    else:
+        response = "perfect"
+
+    return "It's " + response + " out there!"
+result = hows_the_weather(30)
+print(result) 
+
 
 def fizzbuzz(num):
     # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
+print(fizzbuzz(15)) 
+
 
 def calculator(operation, num1, num2):
     # your code here
-    pass
+      if operation == "+":
+        return num1 + num2
+      elif operation == "-":
+        return num1 - num2
+      elif operation == "*":
+        return num1 * num2
+      elif operation == "/":
+        return num1 / num2
+      else:
+        print("Invalid operation!")
+        return None
+print(calculator("+", 5, 3))
